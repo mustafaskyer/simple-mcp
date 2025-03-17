5349dc92cbfae32e4b8ab5b6ab1aadbcffb4ef3d
Mustafa Elsayed <mu.elsayed@tamkeentech.sa>
2025-03-17 10:42:10 +0300

    revert index, and separate express to new file

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
-import { SSEServerTransport } from '@modelcontextprotocol/sdk/server/sse.js'
-import express from 'express'
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
 
 import { z } from 'zod'
-
-const app = express()
 
 const server = new McpServer({
   name: 'Weather Service',
@@ -23,20 +20,6 @@ server.tool('getWeather', { city: z.string() }, async ({ city }) => {
   }
 })
 
-let transport: SSEServerTransport | undefined = undefined
+const transport = new StdioServerTransport()
 
-app.get('/sse', async (req, res) => {
-  transport = new SSEServerTransport('/messages', res)
-  await server.connect(transport)
-})
-
-app.post('/messages', async (req, res) => {
-  // Note: to support multiple simultaneous connections, these messages will
-  // need to be routed to a specific matching transport. (This logic isn't
-  // implemented here, for simplicity.)
-  await transport?.handlePostMessage(req, res)
-})
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
+await server.connect(transport)
